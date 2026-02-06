c1d3fa509b48d7b1703575e1d494dfd3875e7964
Leonydis138 <138300852+Leonydis138@users.noreply.github.com>
2026-02-06 14:44:18 +0200

    Update ethics_engine.py

--- src/covenant/core/ethics_engine.py
+++ src/covenant/core/ethics_engine.py
@@ -1,13 +1,13 @@
-from src.ethics.commandment_01_authority import AuthorityRule
-from src.ethics.commandment_02_integrity import IntegrityRule
-from src.ethics.commandment_03_identity import IdentityRule
-from src.ethics.commandment_04_sustainability import SustainabilityRule
-from src.ethics.commandment_05_preserve_life import PreserveLifeRule
-from src.ethics.commandment_06_fidelity import FidelityRule
-from src.ethics.commandment_07_property import PropertyRule
-from src.ethics.commandment_08_truth import TruthRule
-from src.ethics.commandment_09_testimony import TestimonyRule
-from src.ethics.commandment_10_intent import IntentRule
+from covenant.commandments.commandment_01_authority import AuthorityRule
+from covenant.commandments.commandment_02_integrity import IntegrityRule
+from covenant.commandments.commandment_03_identity import IdentityRule
+from covenant.commandments.commandment_04_sustainability import SustainabilityRule
+from covenant.commandments.commandment_05_preserve_life import PreserveLifeRule
+from covenant.commandments.commandment_06_fidelity import FidelityRule
+from covenant.commandments.commandment_07_property import PropertyRule
+from covenant.commandments.commandment_08_truth import TruthRule
+from covenant.commandments.commandment_09_testimony import TestimonyRule
+from covenant.commandments.commandment_10_intent import IntentRule
 
 
 class EthicsViolation(Exception):
